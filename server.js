94bf5640bc79f3e4eb6daea6204e996b3a2ece9a
Marcelo Silva <marceloamaral.amaral@gmail.com>
2025-02-17 22:13:28 -0800

    return log message format and modify sent message format. - - -
     Changes to be committed:
    	modified:   index.html
    	modified:   server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ io.on('connection', (socket) => {
     const { username, message } = data;
 
     // Log the message and username
-    console.log(`${username}: ${message}`);
+    console.log(`User ${username} sent this message: ${message}`);
 
     // Broadcast the formatted message to all users
     io.emit('receive-message', { username, message });
